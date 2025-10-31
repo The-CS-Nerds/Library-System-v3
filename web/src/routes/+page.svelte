c35a087b99fcfee662374ac98af180974e4f90c2
SuitablyMysterious <fly2be@outlook.com>
2025-10-31 18:07:26 +0000

    Fix copilot suggestions

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -3,7 +3,7 @@
   import * as Button from "$lib/components/ui/button";
   import * as ModeWatcher from "mode-watcher";
 
-  import * as LucideIcons from "@lucide/svelte/icons"
+  import * as LucideIcons from "@lucide/svelte/icons";
 </script>
 
 <nav class="flex items-center justify-between w-full border-b bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4 py-2">
@@ -36,6 +36,6 @@
   <Button.Root variant="outline" onclick=ModeWatcher.toggleMode>
     <LucideIcons.Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0" />
     <LucideIcons.Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100" />
+    <span class="sr-only">Toggle dark-mode</span>
   </Button.Root>
-  <span class="sr-only">Toggle theme</span>
 </nav>
