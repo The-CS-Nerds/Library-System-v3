fc5bbf37eda2c224039a8d4fe536325666e19855
SuitablyMysterious <fly2be@outlook.com>
2025-11-01 18:51:43 +0000

    Update db schema for proper use

--- web/src/lib/server/db/schema.ts
+++ web/src/lib/server/db/schema.ts
@@ -1,16 +1,42 @@
-import { pgTable, serial, integer, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, serial, date, time, integer, text, timestamp } from 'drizzle-orm/pg-core';
+import { title } from 'process';
 
 export const user = pgTable('user', {
-	id: text('id').primaryKey(),
-	age: integer('age')
+	id: text('id').primaryKey().notNull(),
+	firstName: text('firstName'),
+	surname: text('surname'),
+	libraryNumber: integer('libraryNumber').notNull(),
+	email: text('email').unique().notNull(),
+	password: text('password').notNull(),
+	dateAdded: date('dateAdded')
 });
 
 export const session = pgTable('session', {
-	id: text('id').primaryKey(),
+	id: text('id').primaryKey().notNull(),
 	userId: text('user_id').notNull().references(() => user.id),
 	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
+});
+
+export const book = pgTable('book', {
+	id: text('id').primaryKey().notNull(),
+	title: text('title').notNull(),
+	isbn: text('isbn'),
+	dateAdded: date('dateAdded'),
+	description: text('description')
+});
+
+export const loan = pgTable('loan', {
+	id: text('id').primaryKey().notNull(),
+	userId: text('user').notNull().references(() => user.id),
+	dateTakenOut: date('dateTakenOut').notNull(),
+	loanLength: time('loanLength').notNull(),
+	
 });
 
 export type Session = typeof session.$inferSelect;
 
 export type User = typeof user.$inferSelect;
+
+export type Book = typeof book.$inferInsert;
+
+export type Loan = typeof loan.$inferInsert;
